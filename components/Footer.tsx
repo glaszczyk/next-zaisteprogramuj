cbd6177d81b8a8ba5f47ed9cfd78b033901e68e4
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2023-02-24 15:31:16 +0100

    feature/03: Use relative paths for imports

---
+++ components/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => {
+    return (
+      <footer className='bg-gray-400 px-4 py-2'>Stopka</footer>
+    )
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,12 @@
-import {Header} from "@/components/Header";
-import {Footer} from "@/components/Footer";
+import {Header} from '../components/Header';
+import {Footer} from '../components/Footer';
 
 const AboutPage = () => {
     return (
         <div className='flex flex-col min-h-screen max-w-2xl'>
             <Header />
             <main className='bg-gray-50 flex-grow px-4 py-2 grid gap-6 sm:grid-cols-2'>
-                <p>Cześć z nowej strony</p>
+                <p>Hello World!</p>
             </main>
             <Footer />
         </div>
