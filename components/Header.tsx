7a54ef8c9aaf28f09176db346f066d06caf5c430
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2023-02-24 16:10:46 +0100

    feature/04: Reorganize nav order

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,8 @@ export const Header = () => {
   return (
       <header className='mx-auto w-full'>
           <nav className='bg-gray-300 px-4 py-2'>
-              <Link href='/about'>About</Link>
               <Link href='/'>Index</Link>
+              <Link href='/about'>About</Link>
           </nav>
       </header>
   )
