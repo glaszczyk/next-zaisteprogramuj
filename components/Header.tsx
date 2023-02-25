d61cf168cbf80db4f8262adb85f19dff9f249a89
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2023-02-25 22:05:02 +0100

    Feature/04 navbar and navigation (#6)
    * feature/04: Add basic page structure. Extract Header and Footer components
    
    * feature/04: Reorganize nav order
    
    * feature/04: Extract NavLink with selected item class

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+const links = [
+    {label: 'Index', href: '/'},
+    {label: 'About', href: '/about'},
+]
+
+type NavLinkProps = {
+    href: string,
+    isActive: boolean,
+    children: string | JSX.Element
+}
+const NavLink = (props: NavLinkProps) => {
+    const {href, children, isActive} = props;
+    const selected = 'font-bold';
+    return (
+        <Link className={`mr-2 ${isActive ? selected : ''}`} href={href}>{children}</Link>
+    )
+}
+export const Header = () => {
+
+  const router = useRouter();
+  const pathname = router.pathname;
+  return (
+      <header className='mx-auto w-full'>
+          <nav className='bg-gray-300 px-4 py-2'>
+              {links.map((link) => (
+                  <NavLink key={link.href} href={link.href} isActive={pathname === link.href}>{link.label}</NavLink>
+              ))}
+          </nav>
+      </header>
+  )
+}
