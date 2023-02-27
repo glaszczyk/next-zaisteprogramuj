6d7fe60bc261f093adc6796edaf2c81b63fcd572
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2023-02-27 13:16:14 +0100

    feature/05: Reuse Main on About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,14 @@
 import {Header} from '@/components/Header';
 import {Footer} from '@/components/Footer';
+import {Main} from "@/components/Main";
 
 const AboutPage = () => {
     return (
         <div className='flex flex-col min-h-screen max-w-2xl'>
             <Header />
-            <main className='bg-gray-50 flex-grow px-4 py-2 grid gap-6 sm:grid-cols-2'>
+            <Main>
                 <p>Hello World!</p>
-            </main>
+            </Main>
             <Footer />
         </div>
     )
