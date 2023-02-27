6846b92f878e80090dcc7d66b003e17f4e259abc
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2023-02-27 21:51:32 +0100

    feature/06: Add getStaticProps for SSG products page. Add type definition

--- pages/ssg-products.tsx
+++ pages/ssg-products.tsx
@@ -1,7 +1,35 @@
-const SSGProductsPage = () => {
+import {InferGetStaticPropsType} from "next";
+
+const SSGProductsPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+	const {data} = props;
 return (
-    <p>To jest strona wyświetlająca produkty via SSG</p>
+    <p>{data[0].title}</p>
 )
 }
 
+// called when app is building
+export const getStaticProps = async () => {
+	const response = await fetch('https://fakestoreapi.com/products/');
+	const data: StoreApiResponse[] = await response.json();
+	return {
+		props: {
+			data
+		}
+	}
+}
+
 export default SSGProductsPage;
+
+interface  StoreApiResponse {
+	image: string;
+	price: number;
+	rating: {
+		rate: number;
+		count: number;
+	};
+	description: string;
+	id: number;
+	title: string;
+	category: string;
+}
+
