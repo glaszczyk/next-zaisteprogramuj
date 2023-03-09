08988152a5970f7410ec5c5a28ad8ee6f8227dd0
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2023-03-09 13:00:06 +0100

    feature/07: Add images config to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['picsum/photos', "fakestoreapi.com"],
+    deviceSizes: [300, 640, 960, 1080],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
 
 module.exports = nextConfig
