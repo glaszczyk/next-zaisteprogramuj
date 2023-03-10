dc61f619ab8a89854c16c08d86629beb13ce8b8d
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2023-03-10 16:07:31 +0100

    feature/10: Add title with product name to products details component

--- components/ProductDetailsCSR.tsx
+++ components/ProductDetailsCSR.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import {StoreApiResponse} from "@/pages/product-list/[productListId]";
 import {Rating} from "@/components/Rating";
+import Head from "next/head";
 
 interface ProductDetailsCSRProps  {
     data: StoreApiResponse
@@ -10,6 +11,9 @@ export const ProductDetailsCSR = (props: ProductDetailsCSRProps) => {
     const {data} = props;
   return (
       <div className='mt-4 bg-white p-4 flex sm:w-full md:w-[75%] w-[50%] flex-col'>
+          <Head>
+              <title>{data.title}</title>
+          </Head>
           <div className=''>
               <Image
                   loader={({src, width}) => `${src}?w=${width}`}
