12bb9ddeab35d6daa42ddc785c494d56c62ae929
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2023-03-21 07:45:18 +0100

    feature/14: Create types for existing queries

--- codegen.ts
+++ codegen.ts
@@ -1,19 +1,19 @@
-
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clfb0zgvs2e8l01ue9ivv1on7/master",
-  documents: "graphql/*.graphql",
+  schema:
+    'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clfb0zgvs2e8l01ue9ivv1on7/master',
+  documents: 'graphql/*.graphql',
   generates: {
-    "generated/graphql.tsx": {
-      preset: "client",
-      plugins: []
+    'generated/': {
+      preset: 'client',
+      plugins: [],
     },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
-  }
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
 };
 
 export default config;
